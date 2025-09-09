7f408efb6650819fbd6ec5f1687bbeb6cf7177ad
Dmytro Kovalchuk <dmytro.kovalchuk@plvision.eu>
2025-09-09 19:23:52 +0300

    Add dummy source files for task 2
    Signed-off-by: Dmytro Kovalchuk <dmytro.kovalchuk@plvision.eu>

---
+++ task2/calc.h
@@ -0,0 +1,16 @@
+#ifndef CALC_H
+#define CALC_H
+
+int add(int a, int b);
+
+int subtract(int a, int b);
+
+int multiply(int a, int b);
+
+int divide(int a, int b);
+
+int factorial(int n);
+
+int sqroot(int n);
+
+#endif
---
+++ task2/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "calc.h"
+
+int main(void) {
+	printf("%d %d %d\n", add(6, 7), subtract(6, 7), multiply(6, 7));
+
+	return 0;
+}
