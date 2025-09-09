39d0bf2aa7c9db4922eb1fe99b959f9d90d994ac
Dmytro Kovalchuk <dmytro.kovalchuk@plvision.eu>
2025-09-09 19:13:56 +0300

    Initial commit
    Signed-off-by: Dmytro Kovalchuk <dmytro.kovalchuk@plvision.eu>

---
+++ task1/func.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "header.h"
+
+void customPrint(void) {
+	printf("Hello from func.c file!\n");
+
+	return;
+}
---
+++ task1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "header.h"
+
+int main() {
+	printf("Hello world!\n");
+
+	customPrint();
+
+	return 0;
+}
